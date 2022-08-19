c93c3ad2cfa6e02000ac04d7ed959547c2f638f5
davidmarian2012 <davidmarian2012@gmail.com>
2022-08-19 16:32:53 +0300

    on push change detection

--- ang/src/app/userlist/userdetails/userdetails.component.ts
+++ ang/src/app/userlist/userdetails/userdetails.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { User } from 'src/app/user';
 
 @Component({
   selector: 'app-userdetails',
   templateUrl: './userdetails.component.html',
-  styleUrls: ['./userdetails.component.css']
+  styleUrls: ['./userdetails.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserdetailsComponent implements OnInit {
 
